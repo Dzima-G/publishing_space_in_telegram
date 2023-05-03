7ccf90d0e19ab03e2eb25d08296f85cd464b64c4
Dzima-G <dgukalin@gmail.com>
2023-05-03 20:17:29 +0300

    fixed name of environment variables

--- posts_an_image.py
+++ posts_an_image.py
@@ -16,7 +16,7 @@ def publish_image(directory, telegram_token, telegram_chat_id, image, ):
 if __name__ == "__main__":
     load_dotenv()
     telegram_token = os.environ['TELEGRAM_TOKEN']
-    telegram_chat_id = os.environ['CHAT_ID']
+    telegram_chat_id = os.environ['TG_CHAT_ID']
     files_in_dir = os.listdir(image_folder_name)
     parser_post_telegram = argparse.ArgumentParser(description='Введите название файла изображения')
     parser_post_telegram.add_argument('image_file_name', nargs='?', default=random.choice(files_in_dir),
